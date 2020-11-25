5eb24ecd624da1d30e963e22617f89c75464d1e4
LRoGlez <luis.roglez@gmail.com>
2020-11-25 23:07:50 +0100

    commit ahora completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,12 +11,29 @@ const routes: Routes = [
   {
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
-  },  {
+  },
+  {
+    path: 'create-files',
+    loadChildren: () => import('./pages/create-files/create-files.module').then( m => m.CreateFilesPageModule)
+  },
+  {
+    path: 'create-files/:id',
+    loadChildren: () => import('./pages/create-files/create-files.module').then( m => m.CreateFilesPageModule)
+  },
+  {
     path: 'date-time',
     loadChildren: () => import('./pages/date-time/date-time.module').then( m => m.DateTimePageModule)
   },
   {
+    path: 'date-time/:id',
+    loadChildren: () => import('./pages/date-time/date-time.module').then( m => m.DateTimePageModule)
+  },
+  {
     path: 'calendar',
+    loadChildren: () => import('./pages/calendar/calendar.module').then( m => m.CalendarPageModule)
+  },
+  {
+    path: 'calendar/:id',
     loadChildren: () => import('./pages/calendar/calendar.module').then( m => m.CalendarPageModule)
   },
   {
